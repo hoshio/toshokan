2108fe3702090c1d1cd7225e83f168477b57a67f
星野 史晶 <hoshino@hoshino-shiakira-no-MacBook-Air.local>
2014-10-17 22:17:48 +0900

    sampleDB作成

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,24 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table library (
+  id                        bigint not null,
+  name                      varchar(255),
+  create_date               timestamp not null,
+  update_date               timestamp not null,
+  constraint pk_library primary key (id))
+;
+
+create sequence library_seq;
+
+
+
+
+# --- !Downs
+
+drop table if exists library cascade;
+
+drop sequence if exists library_seq;
+
